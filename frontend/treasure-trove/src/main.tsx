8394d8d624178f90b5cc4b1c56d65dcaaaf36b18
Sabo <saleb2k@gmail.com>
2024-03-29 14:36:38 +0100

    feat(frontend): Added TanStack to frontend
    added also Devtools
    
    #25

--- frontend/treasure-trove/src/main.tsx
+++ frontend/treasure-trove/src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {ChakraProvider} from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <ChakraProvider>
-            <App/>
-        </ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider>
+                <App/>
+            </ChakraProvider>
+        </QueryClientProvider>      
     </React.StrictMode>,
 )

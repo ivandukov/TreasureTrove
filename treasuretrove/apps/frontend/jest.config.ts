d6891c96d8e2dea24f1e78a033a5e247adfaf4cc
Sabo <saleb2k@gmail.com>
2024-07-22 16:41:41 +0200

    refactor(frontend): renamed react app to frontend
    #44

--- treasuretrove/apps/frontend/jest.config.ts
+++ treasuretrove/apps/frontend/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'treasuretrove',
+  displayName: 'frontend',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/treasuretrove',
+  coverageDirectory: '../../coverage/apps/frontend',
 };
